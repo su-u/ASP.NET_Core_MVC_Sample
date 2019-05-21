f8668d91e1a36044fac86da4991c824aa7bca824
すー <aaaiiiuuu35@gmail.com>
2019-05-21 11:57:54 +0900

    Create HelloWorldController.cs

---
+++ ASP.NET_Core_MVC_Sample/ASP.NET_Core_MVC_Sample/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ASP.NET_Core_MVC_Sample.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public String Index()
+        {
+            return "This is my default action...";
+        }
+
+        //public String Welcome()
+        //{
+        //    return "This is the Welcome action methd...";
+        //}
+
+        public String Welcome(String name, Int32 numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTime is : {numTimes}");
+        }
+    }
+}
