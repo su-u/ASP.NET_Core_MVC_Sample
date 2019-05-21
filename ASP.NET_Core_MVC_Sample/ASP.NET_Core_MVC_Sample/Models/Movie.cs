18603bd0b22ae716bd852d47edb8236f9f3c268f
すー <aaaiiiuuu35@gmail.com>
2019-05-21 12:26:03 +0900

    Create Movie.cs

---
+++ ASP.NET_Core_MVC_Sample/ASP.NET_Core_MVC_Sample/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET_Core_MVC_Sample.Models
+{
+    public class Movie
+    {
+        public Int32 Id { get; set; }
+        public String Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseData { get; set; }
+        public String Genre { get; set; }
+        public Decimal Price { get; set; }
+
+
+
+    }
+}
