5f9c0f0ceb944fee5b6b86320130a766756e902b
すー <aaaiiiuuu35@gmail.com>
2019-05-21 12:28:33 +0900

    MovieClass Scaffolding

---
+++ ASP.NET_Core_MVC_Sample/ASP.NET_Core_MVC_Sample/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_MVC_Sample.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP.NET_Core_MVC_Sample.Models.Movie> Movie { get; set; }
+    }
+}
